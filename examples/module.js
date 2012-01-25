f0b6de69366565920c630468d2a328d67329b98e
andrew morton <drewish@katherinehouse.com>
2012-01-25 17:38:28 -0500

    Looks like request wants to you explicitly set the method to POST even if you sepecify a body. Adding a simple error message so you get a clue if it fails rather than a blank screen.

--- examples/module.js
+++ examples/module.js
@@ -9,7 +9,8 @@ forever.start(parserproxy).on('start', function (process, data){
   // you are going to make one or more requests immediately upon start up.
 
   setTimeout(function(){
-    request({ uri : 'http://localhost:3030/parseFeed',
+    request({ method : 'POST',
+              uri : 'http://localhost:3030/parseFeed',
               body : { url: 'http://cyber.law.harvard.edu/rss/examples/rss2sample.xml' },
               json : true },
               function (err, response, body){
@@ -18,6 +19,9 @@ forever.start(parserproxy).on('start', function (process, data){
                   body.articles.forEach(function (article) {
                     console.log('%s - %s', article.pubDate, article.title || article.description.substring(0,50));
                   });
+                }
+                else {
+                  console.log("Either couldn't connect to parserproxy or it failed.");
                 }
               });
   }, 1000);
