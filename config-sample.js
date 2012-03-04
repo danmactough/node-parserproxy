94d031e6217eef1d080ab61c528f3e07bb19436b
Dan MacTough <danmactough@gmail.com>
2012-03-04 00:35:26 -0500

    Add optional external configuration file

---
+++ config-sample.js
@@ -0,0 +1,14 @@
+/*!
+ * node-parserproxy - Config sample
+ * Copyright(c) 2011 Dan MacTough <danmactough@gmail.com>
+ * All rights reserved.
+ */
+
+/**
+ * Parserproxy configuration.
+ */
+
+exports = module.exports = {
+  port : 3010
+, timeout : null // Takes a Number
+}
